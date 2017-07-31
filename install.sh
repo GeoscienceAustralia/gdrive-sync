82447829ceb81cd10a50bf403250ba99c514a423
James Kingsmill <james.kingsmill@gmail.com>
2017-07-31 19:42:57 +1000

    Reorder install.sh for debugging

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+# Insert Google token
+sed -i 's#token =#token = '"$GDRIVE_TOKEN"'#' rclone.conf
+
+# Copy rclone config
+mkdir -p $HOME/.config/rclone
+cp rclone.conf $HOME/.config/rclone/rclone.conf
+
 # Download rclone
 wget https://github.com/ncw/rclone/releases/download/v1.37/rclone-v1.37-linux-amd64.zip
 unzip rclone-v1.37-linux-amd64.zip
 mv rclone-v1.37-linux-amd64/rclone .
 
-# Insert Google token
-sed -i 's#token =#token = '"$GDRIVE_TOKEN"'#' rclone.conf
-
-# Copy rclone config
-mkdir -p ~/.config/rclone
-cp rclone.conf ~/.config/rclone/rclone.conf
